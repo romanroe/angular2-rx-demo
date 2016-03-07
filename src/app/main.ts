b5f46021c66ee309496659f6509752ee1cd025e6
Roman Roelofsen <romanroe@gmail.com>
2016-03-07 14:21:30 +0100

    wip

--- src/app/main.ts
+++ src/app/main.ts
@@ -7,50 +7,41 @@ import {Component} from "angular2/core";
 import {Observable} from "rxjs/Observable";
 import {Subject} from "rxjs/Subject";
 
-// ------------------------------------------------------------------
-
-const inputLinks: Subject<string> = Subject.create();
-const inputRechts: Subject<string> = Subject.create();
-
-const inputLinksUpper = inputLinks.map(s => s.toUpperCase());
-const countX: Subject<number> = Subject.create();
-
-const beide = inputLinks.combineLatest(countX.distinctUntilChanged());
-
-// ------------------------------------------------------------------
 
 @Component({selector: "main-app", templateUrl: "app/main.html"})
 export class Main {
 
-    outputLinks = "Hallo Welt!";
-    outputRechts = 0;
+    inputLinks: Subject<string> = Subject.create();
+    inputRechts: Subject<string> = Subject.create();
+
+    inputLinksUpper = this.inputLinks
+        .map(s => s.toUpperCase());
+
+    inputRechtsCountX: Observable<string> = this.inputRechts
+        .debounceTime(1000)
+        .flatMap(value => {
+            var ajax = $.get("http://localhost:8080/countX?text=" + value).promise();
+            return Observable.fromPromise(ajax);
+        });
+
+    kombiniert = this.inputLinksUpper
+        .combineLatest(this.inputRechtsCountX.distinctUntilChanged());
+
+    outputLinks = "";
+    outputRechts = "";
 
     constructor() {
-        inputLinksUpper.subscribe((value: string) => {
+        this.inputLinksUpper.subscribe(value => {
             this.outputLinks = value;
         });
 
-        inputRechts.debounceTime(1000).subscribe((value: string) => {
-            $.get("http://localhost:8080/countX?text=" + value, (result: string) => {
-                countX.next(parseInt(result));
-            });
+        this.inputRechtsCountX.subscribe(value => {
+            this.outputRechts = value;
         });
 
-        countX.subscribe((count: number) => {
-            this.outputRechts = count;
-        });
-
-        beide.subscribe(value => {
+        this.kombiniert.subscribe(value => {
             $("#kombiniert").append("<div>" + value + "</div>");
         });
-    }
-
-    inputLinksChanged(value: string) {
-        inputLinks.next(value);
-    }
-
-    inputRechtsChanged(value: string) {
-        inputRechts.next(value);
     }
 
 }
